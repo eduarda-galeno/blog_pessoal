01de4cfca13f82d3345a469d0ded2954ac655819
Eduarda <contatoeduardagaleno@outlook.com>
2025-01-25 00:34:33 -0300

    Testes - Buscar pelo ID do usuário

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
 import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
+import { Usuario } from './usuario/entities/usuario.entity';
+import { UsuarioModule } from './usuario/usuario.module';
 
 // O app.module defino todos os recursos que terá na aplicação
 
@@ -17,14 +19,15 @@ import { TemaModule } from './tema/tema.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: 'docerosa',
       database: 'db_blogpessoal', // Nome do banco de dados
-      entities: [Postagem, Tema], // Entidades -> Cria tabelas no banco de dados
+      entities: [Postagem, Tema, Usuario], // Entidades -> Cria tabelas no banco de dados
       synchronize: true,  // Verifica se teve alteração na module, atualizando caso tenha (deixa tudo sincronizado)
       logging: true,  // Mostra no console os comandos SQL gerados no Type ORM
     }),
     PostagemModule,
     TemaModule,
+    UsuarioModule
   ],
   controllers: [],   // Controladoras 
   providers: [],  // Registra as classes de serviço
--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -2,6 +2,7 @@ import { Transform, TransformFnParams } from "class-transformer";
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Tema } from "../../tema/entities/tema.entity";
+import { Usuario } from "../../usuario/entities/usuario.entity";
 
 // Está criando a tabela
 @Entity({name: "tb_postagens"}) // CREATE TABLE tb_postagens
@@ -32,7 +33,12 @@ export class Postagem{
     @ManyToOne(() => Tema, (tema) => tema.postagem, {
         onDelete: "CASCADE" // Quando apagar o tema, apagar as postagens conectadas com esse tema
     })
-    tema: Tema
-    usuario: any;
+    tema: Tema;
+
+    // Relacionamento com a entidade Usuario
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+        onDelete: "CASCADE" // Quando apagar o usuário, apagar as postagens associadas a ele
+    })
+    usuario: Usuario;
 
 }
